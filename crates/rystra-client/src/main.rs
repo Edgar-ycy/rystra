3c6a7cb820c899938cb2c76f5e38e15eaccbbd5a
edgar_ye <edgar_ye@petalmail.com>
2026-01-10 00:21:11 +0800

    读取配置文件

--- crates/rystra-client/src/main.rs
+++ crates/rystra-client/src/main.rs
@@ -1,3 +1,7 @@
+use rystra_core::config::common_config::ClientConfig;
+use toml;
 fn main() {
-    println!("Hello, world!");
+    let content = std::fs::read_to_string("config/client.toml").expect("配置文件读取失败");
+    let config: ClientConfig = toml::from_str(&content.as_str()).expect("配置文件解析失败");
+    println!("{:#?}", config);
 }
--- crates/rystra-server/src/main.rs
+++ crates/rystra-server/src/main.rs
@@ -1,1 +1,7 @@
-fn main() {}
+use rystra_core::config::common_config::ServerConfig;
+use toml;
+fn main() {
+    let content = std::fs::read_to_string("config/server.toml").expect("配置文件读取失败");
+    let config: ServerConfig = toml::from_str(&content.as_str()).expect("配置文件解析失败");
+    println!("{:#?}", config);
+}
